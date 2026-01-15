56b62612982f8896478ca20132b4f6dd98f087b6
jeonjyu <jihoojeon77@gmail.com>
2026-01-15 18:09:29 +0900

    [fix] 사용자 id 추가

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -17,21 +17,21 @@ public class RoomManager : MonoBehaviourPunCallbacks
     {
         players = PhotonNetwork.PlayerList;
 
-        foreach (var p in players) Debug.Log(p.NickName);
+        foreach (var p in players) Debug.Log(p.UserId);
 
         if (PhotonNetwork.IsMasterClient == false)
         {
             // 방장이 아니면 할 일
-            _readyBtn.gameObject.SetActive(true);
-            _startBtn.gameObject.SetActive(false);
+            _readyBtn.gameObject?.SetActive(true);
+            _startBtn.gameObject?.SetActive(false);
         }
         else 
         {
-            _readyBtn.gameObject.SetActive(false);
-            _startBtn.gameObject.SetActive(true);
+            _readyBtn.gameObject?.SetActive(false);
+            _startBtn.gameObject?.SetActive(true);
         }
 
-            UpdateUI();
+        UpdateUI();
         // 준비 완료 등 처리
     }
 
@@ -45,7 +45,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        Debug.Log("[RoomManager]" + newPlayer.NickName + " 입장");
+        Debug.Log("[RoomManager]" + newPlayer.UserId + " 입장");
 
         // 플레이어 목록 업데이트
         UpdateUI();
@@ -58,7 +58,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
             Debug.Log("[RoomManager] 방장이 퇴장하여 로비로 이동합니다");
             ExitRoom();
         }
-        Debug.Log("[RoomManager]" + otherPlayer.NickName + " 퇴장");
+        Debug.Log("[RoomManager]" + otherPlayer.UserId + " 퇴장");
         // 플레이어 목록 업데이트
         UpdateUI();
     }
@@ -66,20 +66,30 @@ public class RoomManager : MonoBehaviourPunCallbacks
     // player에 있는 애들을 매번 Instantiate 안하고 불러오기할 수 없나?
     public void UpdateUI()
     {
-        foreach (GameObject child in _listPanel) Destroy(child);
+        Debug.Log("[RoomManager] 리스트 갱신");
+
+        if (_listPanel.childCount > 0)
+        {
+            PlayerSlotView[] slots = _listPanel.GetComponentsInChildren<PlayerSlotView>();
+            foreach (PlayerSlotView slot in slots) Destroy(slot.gameObject);
+        }
 
         //매번 Instantiate 하는 거랑 있는 presenter 만들어두고 그 애들을 불러오는 거 중에 뭐가 낫지
-        foreach (Player player in players) 
+        foreach (Player player in PhotonNetwork.PlayerList) 
         {
             GameObject slot = Instantiate(_playerSlotPrefab, _listPanel);
-            Debug.Log(player.NickName);
-            slot.GetComponent<PlayerSlotView>().Init(player.NickName);
+            Debug.Log("[RoomManager] 리스트 " + player.UserId);
+            slot.GetComponent<PlayerSlotView>().Init(player);
         }
     }
 
     public void ExitRoom()
     {
         PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
         SceneManager.LoadScene(1);
     }
 }
