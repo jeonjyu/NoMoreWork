43aaa1e63a920127f1b818b8996d652e27f02a33
jeonjyu <jihoojeon77@gmail.com>
2026-01-15 18:10:14 +0900

    [add] 프로필 용 UserInfo 클래스 추가
    FirebaseUser 데이터 가공

---
+++ Assets/Scripts/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// FirebaseUser에서 받아온 사용자 데이터를 가공. 
+/// 프로필에 사용.
+/// </summary>
+[Serializable]
+public class UserInfo
+{
+    public string UserName;
+    public string UserId;
+    public string ProfileImgUrl;
+
+    public UserInfo(string userName, string userId, string profileImgUrl = "need profileImgURL")
+    {
+        UserName = userName;
+        UserId = userId;
+        ProfileImgUrl = profileImgUrl;
+    }
+}
