bb0e83a3355312f7254063892487780bedc151ce
jeonjyu <jihoojeon77@gmail.com>
2026-01-19 10:23:00 +0900

    [add] 게임 매니저 추가

---
+++ Assets/Scripts/InGame/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [SerializeField] GameObject playerPrefab;
+
+    private void Start()
+    {
+        instance = this;
+        //플레이어 정보 없으면 코루틴 실행
+        //StartCoroutine(SpawnPlayer());
+    }
+
+    IEnumerator SpawnPlayer()
+    {
+        yield return new WaitUntil(() => PhotonNetwork.InRoom);
+
+        // 네트워크 상에서 생성하기
+    }
+
+    //public override void OnLeftRoom()
+    //{
+    //    SceneManager.LoadScene(1);
+    //}
+
+    public void LeaveRoom()
+    {
+        if (!PhotonNetwork.InRoom) return;
+        if (PhotonNetwork.NetworkClientState == Photon.Realtime.ClientState.Leaving) return;
+        PhotonNetwork.LeaveRoom();
+    }
+}
