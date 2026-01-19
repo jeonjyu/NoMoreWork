ecaa98ab33892929b5ad5477954938ccd0c18242
jeonjyu <jihoojeon77@gmail.com>
2026-01-19 10:23:10 +0900

    [add] 플레이어 매니저 추가

---
+++ Assets/Scripts/InGame/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour, IPunObservable
+{
+    InputAction _playerInput;
+    public static GameObject LocalPlayerInstance;
+
+    private void Start()
+    {
+        // 카메라워크 설정
+    }
+
+    private void Awake()
+    {
+        //_inputAction = InputSystem.actions[];
+    }
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
+}
