bc92c365776faffe070518f6d57f555a1bd88ced
jeonjyu <jihoojeon77@gmail.com>
2026-01-23 12:34:18 +0900

    [add] 플레이어 카메라 설정

---
+++ Assets/Scripts/InGame/Player/PlayerCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    static CinemachineVirtualCamera _virtualCamera;
+
+    private void Start()
+    {
+        Debug.Log("[PlayerCamera] Start");
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    /// <summary>
+    /// 로컬 플레이어의 카메라 설정
+    /// </summary>
+    /// <param name="localplayer">시네머신이 Follow, LookAt 할 대상</param>
+    public static void SetPlayerCamera(Transform localplayer)
+    {
+        if(localplayer == null)
+        {
+            Debug.Log("[PlayerCamera] 플레이어를 찾을 수 없습니다");
+        }
+        _virtualCamera.Follow = localplayer;
+        _virtualCamera.LookAt = localplayer;
+    }
+}
