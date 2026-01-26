a5d40f3196aa1e9b5f130a0fb40b3cbcaf464bd9
jeonjyu <jihoojeon77@gmail.com>
2026-01-26 12:25:44 +0900

    [test] 씬 변경 도구 추가

---
+++ Assets/Scripts/Editor/SceneChanger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class SceneChanger : EditorWindow
+{
+
+    [MenuItem("Tools/Scene Manager/Scene Changer")]
+    public static void ShowWindow()
+    {
+        GetWindow<SceneChanger>("Scene Changer").Focus();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Scene Changer", EditorStyles.boldLabel);
+        EditorGUILayout.Space(5);
+
+        if(GUILayout.Button("로비 씬"))
+        {
+            UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/Scenes/LobbyScene.unity");
+        }
+        
+        EditorGUILayout.Space(5);
+        
+        if(GUILayout.Button("룸 씬"))
+        {
+            UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/Scenes/RoomScene.unity");
+        }
+        
+        EditorGUILayout.Space(5);
+
+        if(GUILayout.Button("인게임 씬"))
+        {
+            UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/Scenes/InGameScene.unity");
+        }
+    }
+}
