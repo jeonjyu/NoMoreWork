bd6ad38035d81982b26b502bb02d1e2bf39699b4
jeonjyu <jihoojeon77@gmail.com>
2026-01-26 12:24:03 +0900

    [test] 플레이 테스트 에디터 윈도우 추가

--- Assets/Scripts/Editor/PlayTestEditor.cs
+++ Assets/Scripts/Editor/PlayTestEditor.cs
@@ -1,79 +1,26 @@
 ﻿using UnityEditor;
-using Photon.Pun;
 using UnityEngine;
 
-public class PlayTestEditor : UnityEditor.EditorWindow
+public class PlayTestEditor: EditorWindow
 {
-    LobbyManager lobbyManager;
-    bool wasPlaying;
-    bool isLogined;
-
-    [MenuItem("Tools/Login Manager/Auto Login")]
+    [MenuItem("Tools/Play Test Manager/Play Test")]
     public static void ShowWindow()
     {
-        GetWindow<PlayTestEditor>("Auto Login").Focus();
+        EditorWindow.GetWindow<PlayTestEditor>("Play Test");
     }
 
     private void OnGUI()
     {
-        EditorGUILayout.LabelField("Runtime Auto Login", EditorStyles.boldLabel);
-        EditorGUILayout.Space(5);
-
-        if(Application.isPlaying == false)
-        {
-            if(GUILayout.Button("플레이"))
-            {
-                UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/Scenes/TitleScene.unity");
-                EditorApplication.EnterPlaymode();
-            }
-
-            return;
-        }
-
-        if(PlayTestManager.Instance == null)
-        {
-            EditorGUILayout.HelpBox("플레이 테스트 매니저를 찾을 수 없습니다", MessageType.Error);
-            return;
-        }
-
-        if (!PhotonNetwork.InRoom)
-        {
-            if (GUILayout.Button("로그인"))
-            {
-                EditorGUILayout.LabelField("로그인 후 로비로 이동", EditorStyles.boldLabel);
-                PlayTestManager.Instance.ExecuteLogin();
-            }
-        }
-
-        EditorGUILayout.Space(5);
-
-        if (PhotonNetwork.InLobby)
-        {
-            EditorGUILayout.LabelField("테스트용 방 생성 후 입장", EditorStyles.boldLabel);
-            if(GUILayout.Button("테스트용 방 생성"))
-            {
-                PlayTestManager.Instance.CreateTestRoom();
-            }
-        }
-
+        EditorGUILayout.LabelField("Play Test");
         EditorGUILayout.Space(10);
 
-        if (GUILayout.Button("로그인 + 방생성"))
+        if(GUILayout.Button("스테이지 클리어"))
         {
-            PlayTestManager.Instance.ExecuteLogin();
-            PlayTestManager.Instance.CreateTestRoom();
+            for(int i = 0; i < StageManager.Instance.puzzleButtons.Count; i++)
+            {
+                StageManager.Instance.UpdateClearedPuzzle(i+1);
+            }
         }
     }
 
-
-
-    private void Update()
-    {
-        if (Application.isPlaying != wasPlaying)
-        {
-            // 에디터 창을 새로 그리는 메서드
-            // 플레이 여부가 변경되었을 때 다시 그려준다
-            Repaint();
-        }
-    }
 }
