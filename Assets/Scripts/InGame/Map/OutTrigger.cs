2bb3e5538a1ad9cdaeaf9cc17d383cb3f19cd3d9
jeonjyu <jihoojeon77@gmail.com>
2026-01-31 18:52:28 +0900

    [add] 스테이지 이동 트리거 추가

---
+++ Assets/Scripts/InGame/Map/OutTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+public class OutTrigger : MonoBehaviour
+{
+    public event Action PlayerEnteredNextStage;
+
+    // Trigger 영역에 들어가면 다음 스테이지로 넘어감
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && PlayerEnteredNextStage != null)
+        {
+            // InitStage 이벤트 발생
+            PlayerEnteredNextStage?.Invoke();
+        }
+    }
+}
