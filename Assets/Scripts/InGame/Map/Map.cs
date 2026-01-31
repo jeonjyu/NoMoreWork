c84fa99ff0f3dd72d158e624f045b0b019025efd
jeonjyu <jihoojeon77@gmail.com>
2026-01-31 18:48:29 +0900

    [add] Map, MapStructure 베이스 클래스 추가

---
+++ Assets/Scripts/InGame/Map/Map.cs
@@ -0,0 +1,13 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Map : ObjectBase
+{
+    public GameObject puzzleObjects;
+    public GameObject mapObjects;
+    public GameObject gate;
+    public OutTrigger outTrigger;
+    public Tilemap tilemap;
+    public CinemachineVirtualCamera virtualCamera;
+}
--- Assets/Scripts/InGame/Map/StageManager.cs
+++ Assets/Scripts/InGame/Map/StageManager.cs
@@ -1,8 +1,12 @@
 ﻿using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using UnityEngine.Tilemaps;
+using Cinemachine;
+using TMPro;
 
 // 스테이지 클리어 
 // 다음 스테이지 생성
@@ -16,16 +20,20 @@ public enum StageState
     Ready, Playing, Clear
 }
 
+// 맵 교체 및 오브젝트 생성
+
 // PuzzleButton의 버튼만 모아서 클리어 여부 확인
 // 스테이지 클리어 판정
-public class StageManager : Singleton<StageManager>, IPunObservable
+public class StageManager : SingletonPun<StageManager>, IPunObservable
 {
+    [Tooltip("현재 오브젝트들을 스폰시킬 스테이지")]
+    public Map currentMap;
+    [SerializeField] GameObject objSpawner;
     [SerializeField] GameObject clearAlertUI;
     [SerializeField] GameObject puzzles;
     public CinemachineVirtualCamera virtualCamera;
 
-    static int _currentStage;
-    int _numOfPuzzles;
+    public int _currentStage = 1;
     bool _isStageClear = false;
     public List<PuzzleButton> puzzleButtons = new List<PuzzleButton>();
     public static Dictionary<int, bool> clearedPuzzle = new Dictionary<int, bool>();
@@ -39,24 +47,81 @@ public class StageManager : Singleton<StageManager>, IPunObservable
 
     void Start()
     {
-        InitStage();
+        // 호스트가 아닌 플레이어도 문으로 이동하면 맵 이동하기 위해 
+        // 이벤트 구독/해제 Init과 별개로 수행하기
+
+        //if (currentMap != null && currentMap.outTrigger != null)
+        //{
+        //    Debug.Log($"[StageManger] outTrigger 이벤트 구독");
+        //    currentMap.outTrigger.PlayerEnteredNextStage += InitStage;
+        //}
+
+        Debug.Log($"[StageManager] 플레이어 수 : {playerCount}");
+        
+        // 맵 오브젝트는 모든 클라이언트가 로컬에 생성
+        SetMapObj();
+        
+        // 호스트가 현재 맵의 설정을 진행 후 각 클라이언트에게 전달
+        if (PhotonNetwork.IsMasterClient)
+        {
+            InitStage();
+            //photonView.RPC(nameof(InitStage), RpcTarget.All);
+        }
+
     }
     private void FixedUpdate()
     {
-        // 딕셔너리에서 clear == true인 애들 갯수랑 전체 갯수 비교
-        // 갯수 동일하면 스테이지 클리어 
-        // FixedUpdate 말고 이벤트로 해도 될 것 같은데
-        
-        if(!_isStageClear && clearedPuzzle.Count == puzzleButtons.Count)
+        // 호스트만 처리하고 RPC로 클리어 여부 쏴주기
+        // 클라이언트는 RPC로 클리어 여부 받아서 클리어 출력 처리
+
+        // activatedPuzzles에서 value가 true인 애들 갯수 비교
+        // 클리어 이벤트 받아서 클리어 처리하는 게 좋을듯
+        //if (!_isStageClear && puzzles.Count == _clearedPuzzleCount)
+        //{
+        //    ClearStage();
+        //}
+    }
+
+    // todo : OutTrigger에 트리거 설정해서 실행시키기
+
+    // 플레이어 제외 오브젝트들 존재할 경우 모두 풀로 집어넣기
+    // 맵 피벗 지정
+    // 맵 피벗따라 맵, 퍼즐 오브젝트 변경
+    [PunRPC]
+    public void InitStage()
+    {
+        Debug.Log($"[StageManager] 스테이지 초기화");
+
+        // 두번째 스테이지 변경시부터 이벤트 구독 취소
+        if (_currentStage != 1)
         {
             ClearStage();
         }
+
+        // 맵 설정
+        currentMap = maps[_currentStage % 2];
+        Debug.Log($"[StageManager] 현재 맵 : {currentMap}({_currentStage} 층)");
+        floorUIText.text = _currentStage + "층";
+
+        ChangeMap(currentMap);
     }
 
-    public void InitStage()
-    { 
-        // 퍼즐 생성하고 번호 부여
-        // 테스트할 때는 인스펙터에서 임의로 지정
+    // 피벗 따라 맵, 퍼즐 기준 변경
+    private void ChangeMap(Map map)
+    {
+        Debug.Log($"[StageManager] 맵 변경");
+        this.puzzleObjects = map.puzzleObjects;
+        this.mapObjects = map.mapObjects;
+        this._gate = map.gate;
+        this.tilemap = map.tilemap;
+        this.virtualCamera = map.virtualCamera;
+        virtualCamera.gameObject.SetActive(true);
+
+        if (currentMap != null && currentMap.outTrigger != null)
+        {
+            Debug.Log($"[StageManger] outTrigger 이벤트 구독");
+            currentMap.outTrigger.PlayerEnteredNextStage += InitStage;
+        }
 
         puzzleButtons.Clear();
         puzzleButtons = puzzles.GetComponentsInChildren<PuzzleButton>().ToList();
