5d20db7ef1fa3dfa1e9c7f89305cd9b6c9e3b021
jeonjyu <jihoojeon77@gmail.com>
2026-01-31 18:35:27 +0900

    [add] PhotonView 싱글톤 추가

---
+++ Assets/Scripts/DesignPattern/Singletone/SingletonPun.cs
@@ -0,0 +1,42 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class SingletonPun<T> : MonoBehaviourPunCallbacks where T : MonoBehaviourPunCallbacks
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindAnyObjectByType<T>();
+
+                if (instance == null)
+                {
+                    GameObject singletoneObject = new GameObject();
+                    instance = singletoneObject.AddComponent<T>();
+                    singletoneObject.name = typeof(T).ToString();
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
