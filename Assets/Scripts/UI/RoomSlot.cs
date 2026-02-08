ab5b4f0dda5b87280d1c79908064c6d550222976
jeonjyu <jihoojeon77@gmail.com>
2026-02-08 19:24:16 +0900

    [add] 방 슬롯 표시용 자료구조 추가

---
+++ Assets/Scripts/UI/RoomSlot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RoomSlot : MonoBehaviour
+{
+    public string roomName;
+    public int currentPlayerCount;
+    public int masterClientId;
+
+    public RoomSlot(string roomName, int curPlayerCnt, int masterId)
+    {
+        this.roomName = roomName;
+        this.currentPlayerCount = curPlayerCnt;
+        this.masterClientId = masterId;
+    }
+}
