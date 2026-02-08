90a19eb4abd6bd6099274a03857e395c62998144
jeonjyu <jihoojeon77@gmail.com>
2026-02-08 19:23:01 +0900

    [add] 게임 클리어시 UI 표시 로직 추가

---
+++ Assets/Scripts/UI/ClearPanel.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Photon.Realtime;
+
+public class ClearPanel : MonoBehaviour
+{
+
+    [SerializeField] Button exitButton;
+    [SerializeField] GameObject playersPanel;
+    [SerializeField] GameObject menuCanvas;
+    TMP_Text[] playerTexts;
+
+    private void Awake()
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        if (menuCanvas.activeInHierarchy)
+            menuCanvas.SetActive(false);
+
+        playerTexts = playersPanel.GetComponentsInChildren<TMP_Text>();
+        Player[] players = PhotonNetwork.PlayerList;
+        
+        for(int i = 0; i < players.Length; i++)
+        {
+            players[i].CustomProperties.TryGetValue("FirebaseName", out object name);
+            playerTexts[i].text = (string)name;
+        }
+    }
+}
