31e111c926fd5d6fb288f5f02fb75c4572a757bc
jeonjyu <jihoojeon77@gmail.com>
2026-02-08 19:17:17 +0900

    [add] 플레이 종료 후 플레이 데이터 저장하기 위한 자료구조 추가
    PlayData : 나중에 JSON으로 활용

---
+++ Assets/Scripts/InGame/PlayData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+public class PlayData
+{
+    public string _name;
+    public int _highScore;
+    public string _date;
+
+    public string Name { get => _name; set => _name = value; }
+    public int HighScore { get => _highScore; set => _highScore = value; }
+    public string Date { get => _date; }
+
+    public PlayData(string name, int highScore)
+    {
+        Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ko-KR");
+        _name = name;
+        _highScore = highScore;
+        _date = DateTime.Now.ToShortDateString();
+    }
+}
