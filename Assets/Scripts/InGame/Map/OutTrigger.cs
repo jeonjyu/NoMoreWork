19a74b6cfc20b223279ccb0ad9595849f94c05c3
jeonjyu <jihoojeon77@gmail.com>
2026-02-08 19:21:50 +0900

    [refactor] out 트리거 발생 조건 추가

--- Assets/Scripts/InGame/Map/OutTrigger.cs
+++ Assets/Scripts/InGame/Map/OutTrigger.cs
@@ -8,10 +8,14 @@ public class OutTrigger : MonoBehaviour
     // Trigger 영역에 들어가면 다음 스테이지로 넘어감
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && PlayerEnteredNextStage != null)
+        if (StageManager.Instance.IsStageClear && StageManager.Instance.CurrentStage < 10)
         {
-            // InitStage 이벤트 발생
-            PlayerEnteredNextStage?.Invoke();
+            if (other.CompareTag("Player") && PlayerEnteredNextStage != null)
+            {
+                // InitStage 이벤트 발생
+                Debug.Log($"[OutTrigger] 다음 스테이지로 가자");
+                PlayerEnteredNextStage?.Invoke();
+            }
         }
     }
 }
